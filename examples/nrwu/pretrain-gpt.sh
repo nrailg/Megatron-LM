27dc73e839a0717954a1eb3a07261cd5c6a063ed
nrwu <nrwu@tencent.com>
2025-04-10 11:23:53 +0800

    demo

---
+++ examples/nrwu/pretrain-gpt.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Runs the "175B" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export GLOO_SOCKET_IFNAME=bond1
+export NCCL_SOCKET_IFNAME=bond1
+
+readonly GPUS_PER_NODE=8
+readonly NODE_RANK="${OMPI_COMM_WORLD_RANK:-0}"
+readonly NNODES="${OMPI_COMM_WORLD_SIZE:-1}"
+readonly WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+readonly MASTER_PORT=65535
+export MASTER_ADDR="${_MASTER_ADDR:-localhost}"
+
+CHECKPOINT_PATH=${PWD}/ckpt
+TENSORBOARD_LOGS_PATH=${PWD}/tb
+VOCAB_FILE=../oscar-data/gpt2-vocab.json
+MERGE_FILE=../oscar-data/gpt2-merges.txt
+DATA_PATH=../oscar-data/my-gpt2_text_document
+
+DISTRIBUTED_ARGS=(
+    --nproc_per_node $GPUS_PER_NODE 
+    --nnodes $NNODES 
+    --node_rank $NODE_RANK
+    --master_addr $MASTER_ADDR 
+    --master_port $MASTER_PORT
+)
+
+GPT_MODEL_ARGS=(
+    --num-layers 4
+    --hidden-size 12288 
+    --num-attention-heads 96 
+    --seq-length 2048 
+    --max-position-embeddings 2048 
+    --attention-backend auto # Can use (flash/fused/unfused/local)
+)
+
+TRAINING_ARGS=(
+    --micro-batch-size 1 
+    --global-batch-size 96
+    --train-iters 500000 
+    --weight-decay 0.1 
+    --adam-beta1 0.9 
+    --adam-beta2 0.95 
+    --init-method-std 0.006 
+    --clip-grad 1.0 
+    --fp16
+    --lr 6.0e-5 
+    --lr-decay-style cosine 
+    --min-lr 6.0e-6
+    --lr-warmup-fraction .001 
+    --lr-decay-iters 430000 
+)
+
+MODEL_PARALLEL_ARGS=(
+	--tensor-model-parallel-size 2
+	--pipeline-model-parallel-size 2
+)
+
+DATA_ARGS=(
+    --data-path $DATA_PATH 
+    --vocab-file $VOCAB_FILE 
+    --merge-file $MERGE_FILE 
+    --split 949,50,1
+)
+
+export WANDB_BASE_URL=https://wandb.lubanml.woa.com
+export WANDB_API_KEY=local-f6267444e79de80aa84dedff1d214b8e090f389b
+
+EVAL_AND_LOGGING_ARGS=(
+    --log-interval 1
+    --save-interval 10000 
+    --eval-interval 1000 
+    --save $CHECKPOINT_PATH 
+    --load $CHECKPOINT_PATH 
+    --eval-iters 10
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH 
+    --wandb-project nrwu
+    --wandb-exp-name no-name
+    --wandb-save-dir ${PWD}/wandb
+)
+
+torchrun ${DISTRIBUTED_ARGS[@]} pretrain_gpt.py \
+    ${GPT_MODEL_ARGS[@]} \
+    ${TRAINING_ARGS[@]} \
+    ${MODEL_PARALLEL_ARGS[@]} \
+    ${DATA_ARGS[@]} \
+    ${EVAL_AND_LOGGING_ARGS[@]}
