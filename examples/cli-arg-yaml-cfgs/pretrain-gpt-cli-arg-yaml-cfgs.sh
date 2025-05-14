d10468a975735ec7c0eb53ef2cf8774626ea90d4
nrwu <nrwu@tencent.com>
2025-05-14 17:07:25 +0800

    drop data

--- examples/cli-arg-yaml-cfgs/pretrain-gpt-cli-arg-yaml-cfgs.sh
+++ examples/cli-arg-yaml-cfgs/pretrain-gpt-cli-arg-yaml-cfgs.sh
@@ -13,6 +13,12 @@ readonly WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 readonly MASTER_PORT=65535
 export MASTER_ADDR="${_MASTER_ADDR:-localhost}"
 
+CHECKPOINT_PATH=${PWD}/ckpt
+TENSORBOARD_LOGS_PATH=${PWD}/tb
+VOCAB_FILE=../oscar-data/gpt2-vocab.json
+MERGE_FILE=../oscar-data/gpt2-merges.txt
+DATA_PATH=../oscar-data/my-gpt2_text_document
+
 DISTRIBUTED_ARGS=(
     --nproc_per_node $GPUS_PER_NODE 
     --nnodes $NNODES 
@@ -21,20 +27,39 @@ DISTRIBUTED_ARGS=(
     --master_port $MASTER_PORT
 )
 
+readonly MODEL_YAML="examples/cli-arg-yaml-cfgs/gpt.yaml"
+
+readonly TRAINING_YAML="examples/cli-arg-yaml-cfgs/trainer.yaml"
+
 MODEL_PARALLEL_ARGS=(
 	--tensor-model-parallel-size 2
 	--pipeline-model-parallel-size 2
 )
 
-CHECKPOINT_PATH=${PWD}/ckpt
-EVAL_AND_LOGGING_ARGS=(
-    --save $CHECKPOINT_PATH 
-    --load $CHECKPOINT_PATH 
+DATA_ARGS=(
+    --data-path $DATA_PATH 
+    --vocab-file $VOCAB_FILE 
+    --merge-file $MERGE_FILE 
+    --split 949,50,1
 )
 
-readonly WORK_DIR="examples/cli-arg-yaml-cfgs"
+export WANDB_BASE_URL=https://wandb.lubanml.woa.com
+export WANDB_API_KEY=local-f6267444e79de80aa84dedff1d214b8e090f389b
+
+EVAL_AND_LOGGING_ARGS=(
+    --log-interval 1
+    --save-interval 10000 
+    --eval-interval 1000 
+    --save $CHECKPOINT_PATH 
+    --load $CHECKPOINT_PATH 
+    --eval-iters 10
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH 
+    --wandb-project nrwu
+    --wandb-exp-name no-name
+    --wandb-save-dir ${PWD}/wandb
+)
 
 torchrun ${DISTRIBUTED_ARGS[@]} pretrain_gpt.py \
-  --cli-arg-yaml-cfgs $WORK_DIR/trainer.yaml $WORK_DIR/data.yaml $WORK_DIR/gpt.yaml \
+  --cli-arg-yaml-cfgs $TRAINING_YAML $MODEL_YAML \
   ${MODEL_PARALLEL_ARGS[@]} \
   ${EVAL_AND_LOGGING_ARGS[@]}
